76ef00ae55400b9570ac9b55d55118ddd1ae0b91
Amit Kumar <toamitkumar@gmail.com>
2012-10-11 16:39:30 +0530

    Using NSOPerationQueue to download the application, showing indicator animation

--- app/controllers/root_view_controller.rb
+++ app/controllers/root_view_controller.rb
@@ -1,11 +1,9 @@
 class RootViewController < UIViewController  
   def viewDidLoad
-    web_view_app = App.delegate.instance_variable_get(:@webview_app)
-    web_view_app.download_and_unzip
+    @web_frame = CGRectMake(0, 0, App.frame.size.height, App.frame.size.width)
 
-    webFrame = CGRectMake(0, 0, App.frame.size.height, App.frame.size.width)
-    @root_web_view = RootWebView.alloc.initWithFrame(webFrame)
-    self.view.addSubview(@root_web_view)    
+    addIndicator
+    pushToQueue
   end
 
   def shouldAutorotateToInterfaceOrientation(interfaceOrientation)
@@ -14,4 +12,45 @@ class RootViewController < UIViewController
     end
     false
   end  
+
+  def pushToQueue
+    p "pushToQueue"
+    queue = NSOperationQueue.new
+    operation = NSInvocationOperation.alloc.initWithTarget(self, selector:"download_and_unzip", object:nil)
+    queue.addOperation(operation)
+  end
+
+  def download_and_unzip
+    p "download_and_unzip"
+    web_view_app = App.delegate.instance_variable_get(:@webview_app)
+    web_view_app.download_and_unzip
+    self.performSelectorOnMainThread("load_web_view", withObject:nil, waitUntilDone:true)
+  end
+
+  def load_web_view
+    @indicatorContainer.removeFromSuperview
+    @root_web_view = RootWebView.alloc.initWithFrame(@web_frame)
+    self.view.addSubview(@root_web_view)
+  end
+
+  def addIndicator
+    @indicatorContainer = UIView.alloc.initWithFrame(@web_frame)
+    @indicatorContainer.backgroundColor = UIColor.colorWithRed(240, green:241, blue:243, alpha:0.9)
+    @activityIndicator = UIActivityIndicatorView.alloc.initWithActivityIndicatorStyle(UIActivityIndicatorViewStyleGray)
+    @indicatorContainer.addSubview(@activityIndicator)
+    @activityIndicator.transform = CGAffineTransformMakeScale(1.5, 1.5)
+    @activityIndicator.frame = CGRectMake((@web_frame.size.width/2)-50, (@web_frame.size.height/2)-70, 100, 100)
+
+    UIView.transitionWithView(
+      self.view, 
+      duration:0.5, 
+      options:UIViewAnimationOptionTransitionCrossDissolve,
+      animations:lambda do
+        # addActivityIndicator(@webFrame)
+        self.view.addSubview(@indicatorContainer)
+        @activityIndicator.startAnimating
+      end, 
+      completion:nil
+    )
+  end
 end
--- lib/webview_app.rb
+++ lib/webview_app.rb
@@ -6,18 +6,14 @@ class WebviewApp
     @name = _name
     @url = _url
     @app_path = "#{App.documents_path}/#{name}"
-    @zip_path = "#{App.documents_path}/#{name}.zip"
+    @zip_path = "#{App.documents_path}/#{name}-zipfile.zip"
   end
 
   def download_and_unzip
     fetch
     delete_at(app_path)
 
-    p zip_path
-
     file_archive = ZKFileArchive.archiveWithArchivePath(zip_path)
-    p file_archive
-    p app_path
     file_archive.inflateToDirectory("#{app_path}1", usingResourceFork:false)
 
     # archivePath = NSBundle.mainBundle.pathForResource(name, ofType:'zip')
@@ -25,12 +21,9 @@ class WebviewApp
 
     # @html_path = NSBundle.mainBundle.pathForResource("index", ofType:"html", inDirectory: name)
     @html_path = "#{app_path}/index.html"
-    p html_path
   end
 
   def fetch
-    p @url
-    p zip_path
     delete_at(zip_path)
     zip_data = NSData.dataWithContentsOfURL(NSURL.URLWithString(@url))
 
